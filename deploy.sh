70ee77d92df35ad29583685b427ff3d2db2476be
Mateusz Tryniszewski <trynio132@gmail.com>
2019-05-23 23:11:43 +0200

    test

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
- git push -f git@github.com:<trynio>/<spa>.git master:gh-pages
+ git push -f git@github.com:<trynio>/<spa/dist>.git master:gh-pages
 
 cd -
